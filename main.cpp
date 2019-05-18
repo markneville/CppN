a54f5cdaa745c41d7acf0718c13baf3c13a14a5f
Mark Neville <mark@markneville.dev>
2019-05-17 23:28:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+void PrintBoard(const vector<vector<int>>& v) {
+    for (auto b : v) {
+        for (int i : b) {
+            cout << i;
+        }
+        cout << "\n";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 0, 0, 0, 1, 0}};
+    PrintBoard(board);
+
 }
